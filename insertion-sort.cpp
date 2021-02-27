88192b41b252973ae939486c20dc1e36268131b4
Abhishek Singh <abhisheksinghxix@gmail.com>
2021-02-27 17:07:55 +0530

    Add another variant of reverse insertion-sort

--- insertion-sort.cpp
+++ insertion-sort.cpp
@@ -17,18 +17,32 @@ void insertion_sort(std::vector<int> &a, int lb = 0, int ub = 0) {
 }
 
 void insertion_sort_rev(std::vector<int> &a, int lb = 0, int ub = 0) {
+  // ub = ub == 0 ? a.size() : ub;
+  // lb = lb == 0 ? lb : lb - 1;
+
+  // for (int i = ub - 2; i >= lb; i--) {
+  //   int key = a.at(i);
+  //   int j = i + 1;
+
+  //   while (j <= ub - 1 && a.at(j) > key) {
+  //     a.at(j - 1) = a.at(j);
+  //     j++;
+  //   }
+  //   a.at(j - 1) = key;
+  // }
+
   ub = ub == 0 ? a.size() : ub;
   lb = lb == 0 ? lb : lb - 1;
 
-  for (int i = ub - 2; i >= lb; i--) {
+  for (int i = lb + 1; i < ub; i++) {
     int key = a.at(i);
-    int j = i + 1;
+    int j = i - 1;
 
-    while (j <= ub - 1 && a.at(j) > key) {
-      a.at(j - 1) = a.at(j);
-      j++;
+    while (j >= lb && a.at(j) < key) {
+      a.at(j + 1) = a.at(j);
+      j--;
     }
-    a.at(j - 1) = key;
+    a.at(j + 1) = key;
   }
 }
 
@@ -43,7 +57,7 @@ int main() {
 
   // insertion_sort(a, 3, 10);
 
-  insertion_sort_rev(a, 4, 8);
+  insertion_sort_rev(a, 3, 10);
   for (auto i : a) {
     std::cout << i << ' ';
   }
